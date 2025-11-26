f3862c5b28e54e238a3c1031a72090b26413d5cc
ChanuRasmika <chanurashmika.edu@gmail.com>
2025-11-26 19:52:58 +0530

    cleaned

--- lib/assimp-6.0.2/build/contrib/zlib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/GV/lib/assimp-6.0.2/contrib/zlib
-# Build directory: D:/GV/lib/assimp-6.0.2/build/contrib/zlib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
